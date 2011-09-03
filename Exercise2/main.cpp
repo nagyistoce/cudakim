e2c8c4a6b0165c3a97e9d8f12cb10fc59c44024d
bjergekim@gmail.com <bjergekim@gmail.com@30b48548-82cf-c5ff-2488-de70f398ffe7>
2011-09-03 12:52:18 +0000

    Exercise2 MacOSX version

---
+++ Exercise2/main.cpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright 1993-2006 NVIDIA Corporation.  All rights reserved.
+ *
+ * NOTICE TO USER:
+ *
+ * This source code is subject to NVIDIA ownership rights under U.S. and
+ * international Copyright laws.
+ *
+ * This software and the information contained herein is PROPRIETARY and
+ * CONFIDENTIAL to NVIDIA and is being provided under the terms and
+ * conditions of a Non-Disclosure Agreement.  Any reproduction or
+ * disclosure to any third party without the express written consent of
+ * NVIDIA is prohibited.
+ *
+ * NVIDIA MAKES NO REPRESENTATION ABOUT THE SUITABILITY OF THIS SOURCE
+ * CODE FOR ANY PURPOSE.  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR
+ * IMPLIED WARRANTY OF ANY KIND.  NVIDIA DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOURCE CODE, INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL,
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+ * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+ * OR PERFORMANCE OF THIS SOURCE CODE.
+ *
+ * U.S. Government End Users.  This source code is a "commercial item" as
+ * that term is defined at 48 C.F.R. 2.101 (OCT 1995), consisting  of
+ * "commercial computer software" and "commercial computer software
+ * documentation" as such terms are used in 48 C.F.R. 12.212 (SEPT 1995)
+ * and is provided to the U.S. Government only as a commercial end item.
+ * Consistent with 48 C.F.R.12.212 and 48 C.F.R. 227.7202-1 through
+ * 227.7202-4 (JUNE 1995), all U.S. Government End Users acquire the
+ * source code with only those rights set forth herein.
+ */
+
+/* Matrix multiplication: C = A * B.
+ * Host code.
+ */
+
+// includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+// includes, project
+// #include <cutil.h>
+
+// includes, kernels
+#include <matrix.h>
+#include <matrixmul_gold.h>
+#include <matrixmul_device.h>
+
+#include <cuda.h>
+#include <cutil.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Program main
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char** argv) {
+
+	Matrix  M;
+	Matrix  N;
+	Matrix  P;
+	int size = 512;
+	
+	srand(52);
+	
+    // Allocate and initialize the matrices
+    //M  = AllocateMatrix(rand() % 1024, rand() % 1024, 1);
+    //N  = AllocateMatrix(M.width, rand() % 1024, 1);
+    //M  = AllocateMatrix(1024, 2048, 1);
+    //N  = AllocateMatrix(M.width, 512, 1);
+    M  = AllocateMatrix(size, size, 1);
+    N  = AllocateMatrix(M.width, size, 1);
+    P  = AllocateMatrix(M.height, N.width, 0);
+
+
+    PrintDeviceProperties();
+	// M * N on the device
+    MatrixMulOnDevice(M, N, P);
+    
+	printf("GPU computation complete\n");
+    // compute the matrix multiplication on the CPU for comparison
+    Matrix reference = AllocateMatrix(P.height, P.width, 0);
+    computeGold(reference.elements, M.elements, N.elements, M.height, M.width, N.width);
+        
+	printf("CPU computation complete\n");
+    // in this case check if the result is equivalent to the expected soluion
+    CUTBoolean res = cutComparefe(reference.elements, P.elements, 
+									P.height*P.width, 0.001f);
+    printf("Test %s\n", (1 == res) ? "PASSED" : "FAILED");
+    
+	// Free matrices
+    FreeMatrix(&M);
+    FreeMatrix(&N);
+    FreeMatrix(&P);
+	return 0;
+}
+
