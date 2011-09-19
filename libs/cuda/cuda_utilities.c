190c70343e1cf37d66ded2687a18661ebcea1198
bjergekim@gmail.com <bjergekim@gmail.com@30b48548-82cf-c5ff-2488-de70f398ffe7>
2011-09-19 12:43:24 +0000

    Added libs and thrust 1.4.0

---
+++ libs/cuda/cuda_utilities.c
@@ -0,0 +1,19 @@
+#include <cuda/cuda_utilities.h>
+
+#include <common/error.h>
+
+/**
+ *  Should never be used in the code, use CHECK_FOR_CUDA_ERROR(); instead
+ */
+void CHECK_FOR_CUDA_ERROR_FUNCTION(const char* file, const char* line) {
+	cudaError_t errorCode = cudaGetLastError();
+	if (errorCode != cudaSuccess) {
+		exit_with_error("[file:%s line:%s] CUDA Error:", 
+                        file, line, cudaGetErrorString(errorCode) );
+	}
+    errorCode = cudaThreadSynchronize();
+    if (errorCode != cudaSuccess) {
+		exit_with_error("[file:%s line:%s] CUDA sync Error:", 
+                        file, line, cudaGetErrorString(errorCode) );
+    }
+}
